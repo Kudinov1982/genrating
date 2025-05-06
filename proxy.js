dbfd27209844ca564672016d7e49b34a52174f12
Kudinov1982 <dm.kudinov@gmail.com>
2025-05-07 00:54:25 +0300

    Add OAuth token to proxy.js

--- proxy.js
+++ proxy.js
@@ -12,12 +12,13 @@ app.use((req, res, next) => {
 
 app.post('/proxy', async (req, res) => {
     try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbyOeqmRR7EgCewXgiSIFE944FhUtKlBUfWRQo2_P1U6WuBfVKWmfi18iPgdqKY9c_PC/exec', {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbxLBKTsm3dIw7URZG510TzgD66D1NAudPrY7hlOkWHGehURfTiFITWiASgEBp_oTIp4/exec', {
             method: 'POST',
             headers: { 
                 'Content-Type': 'application/json',
+                'Authorization': 'Bearer ya29.a0AZYkNZh8u-otIPsdD7u8ffWlyJ5gjMM8gb8JWkGxfa76SkiDJT4z2lKAhcrI1-3Oq14Aro16G_hBvdr2LuPEKnapV19uALkVHPZbXa3tunST898A1b9z3a_fts0bT8I6461rxzU2QDvZmVbFmOk-2a15mOX6m5TYnAUFtenPaCgYKAXUSARUSFQHGX2MiibuPaiH4IT_YLLwj83w3qA0175',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-                'Origin': 'https://your-tilda-page.tilda.ws'
+                'Origin': 'https://metrics.tilda.ws/rating'
             },
             body: JSON.stringify(req.body)
         });
@@ -30,8 +31,9 @@ app.post('/proxy', async (req, res) => {
 
 app.get('/proxy', async (req, res) => {
     try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbyOeqmRR7EgCewXgiSIFE944FhUtKlBUfWRQo2_P1U6WuBfVKWmfi18iPgdqKY9c_PC/exec', {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbxLBKTsm3dIw7URZG510TzgD66D1NAudPrY7hlOkWHGehURfTiFITWiASgEBp_oTIp4/exec', {
             headers: { 
+                'Authorization': 'Bearer ya29.a0AZYkNZh8u-otIPsdD7u8ffWlyJ5gjMM8gb8JWkGxfa76SkiDJT4z2lKAhcrI1-3Oq14Aro16G_hBvdr2LuPEKnapV19uALkVHPZbXa3tunST898A1b9z3a_fts0bT8I6461rxzU2QDvZmVbFmOk-2a15mOX6m5TYnAUFtenPaCgYKAXUSARUSFQHGX2MiibuPaiH4IT_YLLwj83w3qA0175',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
                 'Origin': 'https://metrics.tilda.ws/rating'
             }
